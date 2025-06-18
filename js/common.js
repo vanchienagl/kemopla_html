4163efb123dc4dc495d8767c6952d536fcdec217
chien.nguyen <nguyen_van_chien@allgrow-labo.jp>
2025-06-18 17:47:37 +0700

    footer pc

---
+++ js/common.js
@@ -0,0 +1,9 @@
+// ______________________________________________________
+// ______________________________________________________
+// Button Back To Top
+$(function () {
+  const $backToTopBtn = $(".js_page_to_top");
+  $backToTopBtn.on("click", function () {
+    $("html, body").animate({ scrollTop: 0 }, "smooth");
+  });
+});
