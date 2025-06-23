95de09586f84b29ee730b4ec63219d373b748e6c
chien.nguyen <nguyen_van_chien@allgrow-labo.jp>
2025-06-23 10:24:21 +0700

    fix page lineup

---
+++ js/lineup.js
@@ -0,0 +1,17 @@
+// Clear Button
+$(function () {
+  const clearBtn = document.querySelector('.js_btn_clear');
+  const form = document.querySelector('.js_form_filter');
+
+  clearBtn.addEventListener('click', () => {
+    // Textbox
+    form.querySelectorAll('input[type="text"]').forEach(el => {
+      el.value = '';
+    });
+
+    // Checkbox
+    form.querySelectorAll('input[type="checkbox"]').forEach(el => {
+      el.checked = false;
+    });
+  });
+});
